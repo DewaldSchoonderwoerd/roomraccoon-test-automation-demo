1e20077e73c42db9b1ffe5b946fe84534c464eb2
DewaldSchoonderwoerd <dewald.schoonderwoerd@gmail.com>
2023-08-15 16:27:06 +0200

    Added base test class

---
+++ src/main/java/com/roomraccoon/test/automation/framework/constants/Environments.java
@@ -0,0 +1,8 @@
+package com.roomraccoon.test.automation.framework.constants;
+
+public class Environments {
+
+    public static final String QA = "QA";
+    public static final String STAGING = "STAGING";
+
+}
---
+++ src/main/java/com/roomraccoon/test/automation/framework/constants/PlatformTypes.java
@@ -0,0 +1,8 @@
+package com.roomraccoon.test.automation.framework.constants;
+
+public class PlatformTypes {
+
+    public static final String WEB = "WEB";
+    public static final String API = "API";
+
+}
